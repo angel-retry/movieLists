9a4b9c3b8ed2e3d7d77b9c3087d2d04de7983adc
a27657060 <a27657060@gmail.com>
2024-03-06 14:58:52 +0800

    build: use express-handlebars as view engine

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+
+// 模板引擎
+// 告訴Express遇到附檔名.hbs，就使用指定模板引擎來渲染他
+app.engine('.hbs', engine({ extname: '.hbs' }));
+// 設置Express應用程式的模板引擎為Handlebars
+app.set('view engine', '.hbs');
+// 設置Express應用程式的模板引擎目錄為./views
+app.set('views', './views');
 
 // 取得靜態網站
 app.use(express.static('public'))
@@ -12,7 +21,7 @@ app.get('/', (req, res) => {
 
 // 首頁:電影列表
 app.get('/movies', (req, res) => {
-  res.send('listing movies')
+  res.render('index')
 })
 
 // 取得單一電影資訊
