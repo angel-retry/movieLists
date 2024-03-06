5c37194c10317a3ae8a8a41016c89601afe35963
a27657060 <a27657060@gmail.com>
2024-03-06 14:13:30 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// 首頁
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.redirect('/movies')
+})
+
+// 首頁:電影列表
+app.get('/movies', (req, res) => {
+  res.send('listing movies')
+})
+
+// 取得單一電影資訊
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  res.send(`render movie: ${id}`)
 })
 
 app.listen(port, () => {
