d5d4d381b6138d86cbb2a51ab53eefae51a17d48
a27657060 <a27657060@gmail.com>
2024-03-06 14:26:19 +0800

    build: add static files & json for model

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// 取得靜態網站
+app.use(express.static('public'))
+
 // 首頁
 app.get('/', (req, res) => {
   res.redirect('/movies')
